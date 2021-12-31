8940ddd60b1533c1b68509fb91bcbd32aae2657c
Gihan1937 <gihansen0216@gmial.com>
2021-12-31 00:36:33 -0500

    Assign

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -1,6 +1,10 @@
 function greetFriend(name) {
   // Insert code here;
+
+  return "Hello, " + name + "!";
 }
+
+//console.log(greetFriend("Ben"));
 
 // Do not edit this line;
 module.exports = greetFriend;
--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,14 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  // Insert code here;'
+  var separate = title.toLowerCase().split(" ");
+   for (var i = 0; i < separate.length; i++) {
+      separate[i] = separate[i].charAt(0).toUpperCase() + separate[i].substring(1);
+   }
+   return separate.join(" ");
+  
+
 }
 
+//console.log(titleCaseEdit("the cow jumped over the moon"));
 // Do not edit this line;
 module.exports = titleCaseEdit;
